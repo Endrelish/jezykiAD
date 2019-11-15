e33fb94aef11f95bdf5be49d91f8a514992f1d93
Bartlomiej Ciach <203860@edu.p.lodz.pl>
2019-11-15 23:37:51 +0100

    Ex. 2 Mean imputation, comparison after it

--- zad2.py
+++ zad2.py
@@ -1,3 +1,5 @@
+import statistics
+
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -51,6 +53,63 @@ plt.plot(x, y, '-r')
 plt.title('Graph of linear regression')
 plt.xlabel('x', color='#1C2833')
 plt.ylabel('y', color='#1C2833')
-plt.legend(loc='upper left')
+plt.grid()
+plt.show()
+
+
+##dane przed uzupełnieniem:
+mean = data['TOEFL_Score'].mean()
+st_dev = statistics.stdev(data_without_nans['TOEFL_Score'])
+quantile = data.TOEFL_Score.quantile([0.25, 0.5, 0.75])
+
+## Uzupełnienie danych metoda imputation;
+filled_data = data.copy()
+filled_data['TOEFL_Score'] = data['TOEFL_Score'].fillna(mean)
+
+
+##dane po uzupełnieniu:
+mean_after_fill = filled_data['TOEFL_Score'].mean()
+st_dev_after_fill = statistics.stdev(filled_data['TOEFL_Score'])
+quantile_after_fill = filled_data.TOEFL_Score.quantile([0.25, 0.5, 0.75])
+
+print()
+print('Srednia przed: ', mean)
+print('Srednia po: ', mean_after_fill)
+print('Roznica: ', abs(mean_after_fill - mean))
+print()
+
+print('Odchylenie standardowe przed: ', st_dev)
+print('Odchylenie standardowe po: ', st_dev_after_fill)
+print('Roznica: ', abs(st_dev_after_fill - st_dev))
+print()
+
+print('Kwantyle przed: ', quantile)
+print('Kwantyle po: ', quantile_after_fill)
+print('Roznica: ', abs(quantile_after_fill - quantile))
+
+
+#regracja po imputancji
+X_2 = filled_data[['TOEFL_Score']]
+Y_2 = filled_data['CGPA']
+model_2 = LinearRegression().fit(X_2, Y_2)
+r_sq_2 = model.score(X_2, Y_2)
+print('R^2 po imputancji:', r_sq_2)
+print('intercept po imputancji:', model_2.intercept_)
+print('slope po imputancji:', model_2.coef_)
+print()
+print('Różnica R^2:', abs(r_sq - r_sq_2))
+print('Różnica intercept:', abs(model.intercept_ - model_2.intercept_))
+print('Różnica slope:', abs(model.coef_ - model_2.coef_))
+print()
+
+
+
+
+x2 = np.linspace(0, 50, 100)
+y2 = model_2.coef_*x+model_2.intercept_
+plt.plot(x2, y2, '-r')
+plt.title('Graph of linear regression after imputation')
+plt.xlabel('x', color='#1C2833')
+plt.ylabel('y', color='#1C2833')
 plt.grid()
 plt.show()
